14df2e810416d817a2b2012e6bcf825e51a97894
Alteza13 <antirusher13@gmail.com>
2026-02-08 18:15:35 +0500

    work in process

--- src/main/java/com/example/musiclibrary/MusiclibraryApplication.java
+++ src/main/java/com/example/musiclibrary/MusiclibraryApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusiclibraryApplication {
+public class MusicLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusiclibraryApplication.class, args);
-	}
-
+public static void main(String[] args) {
+	SpringApplication.run(MusicLibraryApplication.class, args);
+}
 }
---
+++ src/main/java/com/example/musiclibrary/model/Media.java
@@ -0,0 +1,25 @@
+package com.example.musiclibrary.model;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
